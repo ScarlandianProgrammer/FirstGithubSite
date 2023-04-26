91f23307322f874a30935d1b32b861b948984ef1
Elias McKee <eqbookfan@gmail.com>
2023-04-26 15:41:09 -0700

    added ccs for the site, finished js for forms page

--- js/site.js
+++ js/site.js
@@ -1,3 +1,17 @@
-function main() {
-    alert("Hello World!");
+window.onload = function () {
+    let submitButton = document.getElementById("submit");
+    submitButton.onclick = submission;
+};
+function submission() {
+    let userExperience = document.getElementById("experience").value;
+    if (userExperience < 3) {
+        alert("That's uncalled for, cretan.");
+    }
+    else if (userExperience < 7) {
+        alert("Understandable. At least you're honest.");
+    }
+    else {
+        alert("You don't have to lie to me, I know it wasn't that good...");
+    }
+    document.getElementById("form").reset();
 }
--- ts/site.ts
+++ ts/site.ts
@@ -1,5 +1,17 @@
-// just making sure the setup works.
-function main() {
-    alert("Hello World!");
-    // just a test
+// making a window.onload to attach a click event
+window.onload = function() {
+    let submitButton = document.getElementById("submit");
+    submitButton.onclick = submission;
+}
+
+function submission() {
+    let userExperience:number = <number>document.getElementById("experience").value;
+    if (userExperience < 3) {
+        alert("That's uncalled for, cretan.");
+    } else if (userExperience < 7) {
+        alert("Understandable. At least you're honest.");
+    } else {
+        alert("You don't have to lie to me, I know it wasn't that good...");
+    }
+    document.getElementById("form").reset();
 }
